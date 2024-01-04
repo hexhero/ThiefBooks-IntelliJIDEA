833e2897b9e76728fc65d0dda133243edc0d89ee
Leo <523084685@qq.com>
2024-01-04 14:18:15 +0800

    Support-2023.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.10.1"
+//    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.intellij") version "1.16.1"
 }
 
 group = "leo.yang.theifbooks"
-version = "1.21"
+version = "1.22"
 
 repositories {
     maven { setUrl("https://maven.aliyun.com/repository/public/") }
+    maven { setUrl("https://maven.aliyun.com/repository/spring/") }
+//    jcenter()
     mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.1.4")
+    version.set("2023.1.5")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -23,13 +26,16 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
+//    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//        kotlinOptions.jvmTarget = "17"
+//    }
 
     patchPluginXml {
-        sinceBuild.set("221")
-        untilBuild.set("231.*")
+        sinceBuild.set("231")
+        untilBuild.set("241.*")
     }
 
     signPlugin {
